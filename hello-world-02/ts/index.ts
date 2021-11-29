04d57b227e8040b41aaa36c40eb4dcfe76eab6ee
Romeu Junior <romeu.junior@gmail.com>
2021-11-29 19:37:23 -0300

    - Work in progress;

---
+++ hello-world-02/ts/index.ts
@@ -0,0 +1,16 @@
+﻿async function main(): Promise<void> {
+    console.log('Meu primeiro programa em TypeScript em NodeJs!');
+
+    for (let x = 1; x <= 10; x++) {
+      console.log();
+      console.log(`Tabuada do ${x}:`);
+      for (let y = 1; y <= 10; y++) {
+        const value = x * y;
+        console.log(`  ${x} * ${y} = ${value}`);
+      }
+    }
+}
+
+main().catch((e) => {
+    throw e;
+});
